b1efd42a33a02d90ee8aaac889decf42ade5a702
Sakthi636 <sakthivelsaravanan636@gmail.com>
2023-09-16 18:25:49 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,55 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      width: {
+        100:"100%",
+        90:"90%",
+        80:"80%",
+        60:"60%",
+        50:"50%"        
       },
+      boxShadow: {
+        lightGray: "0px 4px 16px 0px rgba(126, 153, 193, 0.20)",
+      },
+      colors: {
+        BlueBtn_HoverColor: "#0B4BAA",
+        BlueTextWhiteBtn_HoverColor: "#EBF2FD",
+        WhiteTextDarkBlueBtn_HoverColor: "#3E4C60",
+        BlackTextWhiteBtn_Hovercolor: "#F2F5F9",
+        NavigationBtns_HoverColor: "#EBF2FD",
+        BlueTextLink_HoverColor: "#EBF2FD",
+      },
+      textColor:{
+        default :"#000",
+      },
+      fontSize: {             
+        'fs-14': '14px',
+        'fs-16': '16px',
+        'fs-18': '18px',
+        'fs-20': '20px',
+        'fs-24': "24px",
+        'fs-22': '22px',
+        'fs-24': '24px',
+        'fs-28': '28px'
+      },
+      fontWeight: {        
+        xl:"700",
+        l:"500",
+        normal:"400",        
+      }
     },
+    screens: {
+        'md'  : {'max':"1023px"},   
+        'sm'  : {'max':"650px"},   
+        // 'sm'  : {'max':"400px"}     
+    },
+    aspectRatio: {
+      '16/10': '16/10',
+      '4/4' : "4/4"
+    }
   },
   plugins: [],
-}
+  important: true,
+};
