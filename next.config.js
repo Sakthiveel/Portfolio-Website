fda5648deb2e95f346b23a4c57ab51ac501884d7
Sakthi636 <sakthivelsaravanan636@gmail.com>
2023-09-16 18:26:10 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: false,
+    images: {
+        formats: [
+            "image/avif", "image/webp"
+        ],
+        domains: ["cdn.mos.cms.futurecdn.net"],
+        deviceSizes: [
+            320,
+            640,
+            768,
+            1024,
+            1280,
+            1440,
+            1920
+        ]
+
+    }
+}
 
 module.exports = nextConfig
